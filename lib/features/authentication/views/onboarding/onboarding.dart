e151691d445ca001f13a678aacdfc1d60412a134
syaha-creator <msyahrul090@gmail.com>
2024-11-27 22:54:04 +0700

    Update Features Authentication

---
+++ lib/features/authentication/views/onboarding/onboarding.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tstore/features/authentication/controllers.onboarding/onboarding_controller.dart';
+import 'package:tstore/features/authentication/views/onboarding/widgets/onboarding_dot_navigation.dart';
+import 'package:tstore/features/authentication/views/onboarding/widgets/onboarding_next.dart';
+import 'package:tstore/features/authentication/views/onboarding/widgets/onboarding_page.dart';
+import 'package:tstore/features/authentication/views/onboarding/widgets/onboarding_skip.dart';
+import 'package:tstore/utils/constants/image_strings.dart';
+import 'package:tstore/utils/constants/text_strings.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Horizontal Scrollable Pages
+          PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
+            children: const [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
+
+          // Skip Page
+          const OnBoardingSkip(),
+
+          // Dot Navigation SmoothPageIndicator
+          const OnBoardingDotNavigation(),
+
+          // Circular Button
+          const OnBoardingNextButton(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/views/onboarding/widgets/onboarding_next.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:tstore/features/authentication/controllers.onboarding/onboarding_controller.dart';
+import 'package:tstore/utils/constants/colors.dart';
+import 'package:tstore/utils/constants/sizes.dart';
+import 'package:tstore/utils/device/device_utility.dart';
+import 'package:tstore/utils/helpers/helper_functions.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        onPressed: () => OnBoardingController.instance.nextPage(),
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          backgroundColor: dark ? TColors.primary : Colors.black,
+        ),
+        child: const Icon(Iconsax.arrow_right3),
+      ),
+    );
+  }
+}
