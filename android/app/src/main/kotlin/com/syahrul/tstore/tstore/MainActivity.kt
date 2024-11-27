7e2981276fc7b806e935ee4fbd1c87852c37f147
syaha-creator <msyahrul090@gmail.com>
2024-11-27 12:12:40 +0700

    Make Theme App

---
+++ android/app/src/main/kotlin/com/syahrul/tstore/tstore/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.syahrul.tstore.tstore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/theme/custom_themes/outlined_button_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TOutlineButtonTheme {
+  TOutlineButtonTheme._();
+
+  static final lightOutlineButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      padding: const EdgeInsets.symmetric(vertical: 18),
+      textStyle: const TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+
+  static final darkOutlineButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      padding: const EdgeInsets.symmetric(vertical: 18),
+      textStyle: const TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+}
