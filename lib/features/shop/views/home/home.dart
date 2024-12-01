c55e3e78d40a68a903c7aa6687980bd47104c03e
syaha-creator <msyahrul090@gmail.com>
2024-12-01 23:26:35 +0700

    Update Home Screen Appbar

--- lib/features/shop/views/home/home.dart
+++ lib/features/shop/views/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tstore/features/shop/views/home/widgets/home_appbar.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
@@ -7,12 +8,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Container(),
+              child: Column(
+                children: [
+                  // Appbar
+                  THomeAppBar(),
+                ],
+              ),
             ),
           ],
         ),
