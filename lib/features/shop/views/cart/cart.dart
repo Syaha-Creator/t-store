d1954e0404da3b8fc93177dae565677184f06a88
syaha-creator <msyahrul090@gmail.com>
2024-12-10 11:32:27 +0700

    Add Checkout Page

--- lib/features/shop/views/cart/cart.dart
+++ lib/features/shop/views/cart/cart.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:get/get.dart';
 import 'package:tstore/common/widgets/appbar/appbar.dart';
-import 'package:tstore/common/widgets/icons/t_circular_icon.dart';
-import 'package:tstore/common/widgets/images/t_rounded_image.dart';
-import 'package:tstore/common/widgets/texts/brand_title_text_with_verified.dart';
-import 'package:tstore/common/widgets/texts/product_price_text.dart';
-import 'package:tstore/common/widgets/texts/product_title_text.dart';
-import 'package:tstore/utils/helpers/helper_functions.dart';
+import 'package:tstore/features/shop/views/cart/widgets/cart_items.dart';
+import 'package:tstore/features/shop/views/checkout/checkout.dart';
 
-import '../../../../common/widgets/products/cart/add_remove_button.dart';
-import '../../../../common/widgets/products/cart/cart_item.dart';
-import '../../../../utils/constants/colors.dart';
-import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/sizes.dart';
 
 class CartScreen extends StatelessWidget {
@@ -24,39 +16,15 @@ class CartScreen extends StatelessWidget {
           showBackArrow: true,
           title:
               Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
-      body: Padding(
-        padding: const EdgeInsets.all(TSizes.defaultSpace),
-        child: ListView.separated(
-          shrinkWrap: true,
-          itemCount: 10,
-          separatorBuilder: (_, __) =>
-              const SizedBox(height: TSizes.spaceBtwSections),
-          itemBuilder: (_, index) => const Column(
-            children: [
-              TCartItem(),
-              SizedBox(height: TSizes.spaceBtwItems),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      // Extra Space
-                      SizedBox(width: 70),
-                      // Add Remove Buttons
-                      TProductQuantityWithAddRemoveButton(),
-                    ],
-                  ),
-                  TProductPriceText(price: '256')
-                ],
-              )
-            ],
-          ),
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        child: TCartItems(),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
         child: ElevatedButton(
-            onPressed: () {}, child: const Text('Checkout \$256.0')),
+            onPressed: () => Get.to(() => const CheckoutScreen()),
+            child: const Text('Checkout \$256.0')),
       ),
     );
   }
---
+++ lib/features/shop/views/checkout/widgets/billing_payment_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:tstore/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:tstore/common/widgets/texts/section_heading.dart';
+import 'package:tstore/utils/helpers/helper_functions.dart';
+
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+
+class TBillingPaymentSection extends StatelessWidget {
+  const TBillingPaymentSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Column(
+      children: [
+        TSectionHeading(
+            title: 'Payment Method', buttonTitle: 'Change', onPressed: () {}),
+        const SizedBox(height: TSizes.spaceBtwItems / 2),
+        Row(
+          children: [
+            TRoundedContainer(
+              width: 60,
+              height: 35,
+              backgroundColor: dark ? TColors.light : TColors.white,
+              padding: const EdgeInsets.all(TSizes.sm),
+              child: const Image(
+                  image: AssetImage(TImages.paypal), fit: BoxFit.contain),
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            Text('Paypal', style: Theme.of(context).textTheme.bodyLarge)
+          ],
+        )
+      ],
+    );
+  }
+}
