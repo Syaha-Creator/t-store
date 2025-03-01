34473431297c0d1446e46c8c9b0fbb66300c4f54
Syaha Creator <msyahrul090@gmail.com>
2025-03-01 14:24:01 +0700

    Fix Firebase Connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tstore/app.dart';
+import 'package:tstore/firebase_options.dart';
 
 Future<void> main() async {
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
-  //     .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const App());
 }
