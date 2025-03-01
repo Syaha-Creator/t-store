19953dedc0550a15528021ea43b6189ae7257671
Syaha Creator <msyahrul090@gmail.com>
2025-03-01 14:32:23 +0700

    Fix Firebase Connection

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import 'package:tstore/app.dart';
 import 'package:tstore/firebase_options.dart';
 
 Future<void> main() async {
+  // Widgets Binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  // Load all the Material Design / Themes / Localization / Bindings
   runApp(const App());
 }
